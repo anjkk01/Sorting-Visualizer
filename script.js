7c449178514b060e2f98be633fb0b91684e36603
anjkk01 <121030480+anjkk01@users.noreply.github.com>
2023-06-08 19:29:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,39 @@ function init(){
     }
     showBars();
 }
-function play(array){
-    bubbleSort(array);
+function play(){
+    const temp =  [...arr];
+    const swaps = bubbleSort(temp);
+    // showBars();
+    animate(swaps);
+}
+function animate(swaps){
+    if(swaps.length==0){
+        return;
+    }
+
+    const [i,j]=swaps.shift();
+    [arr[i],arr[j]]=[arr[j],arr[i]];
     showBars();
+    setTimeout(function(){
+        animate(swaps);
+    },50);
+
 }
 function bubbleSort(array){
+const swaps=[];
 while(1){
     var x = false;
     for(let i = 1;i<n;i++){
-        if(arr[i]<arr[i-1]){
+        if(array[i]<array[i-1]){
             x = true;
-            var temp = arr[i];
-            arr[i]=arr[i-1];
-            arr[i-1]=temp;
+            swaps.push([i-1,i]);
+            [array[i-1],array[i]]=[array[i],array[i-1]];
         }
     }
-    if(x==false){
-        break;
-    }
+    if(x==false)break;
 }
+return swaps;
 }
 function showBars(){
     container.innerHTML="";
